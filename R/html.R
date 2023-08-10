bb6133c28a64236d12711c3c88f028377745d944
Joan Maspons <joanmaspons@gmail.com>
2023-08-10 14:18:37 +0200

    Simplifica el codi per mostrar les versions amb canvis
    També gestiona casos en què l'estat de referència no correspon a cap
    versió prèvia.

--- R/html.R
+++ R/html.R
@@ -25,24 +25,54 @@ canvis_html <- function(x) {
 }
 
 
-#' Mostra les versions que introdueixen canvis en html
+#' Formata les versions que introdueixen canvis amb `kable`
 #'
 #' @param x Un dels elements de la llista retornada per [cerca_versio_canvis()].
 #' @param titol Títol de la taula. S'espera una url i se li afegiran les etiquetes \<a href=titol\>titol</a>
 #'
-#' @return La taula html amb informació del conjunt de canvis que ha modificat etiquetes.
+#' @return La taula `knitr_kable` amb informació del conjunt de canvis que ha modificat etiquetes.
+#' @noRd
+versio_canvi_kable <- function(x, titol) {
+  titol_valid <- !missing(titol) || !is.null(titol) || !is.na(titol)
+  if (nrow(x) == 0) {
+    out <- "No s'ha trobat cap versió en l'estat de referència"
+    if (titol_valid) {
+      out <- paste0(out, " per <a href=\"", titol, "\">", titol, "</a>.")
+    } else {
+      out <- paste0(out, ".")
+    }
+  } else {
+    x$changeset <- paste0("<a href=\"https://osm.org/changeset/", x$changeset, "\">", x$changeset, "</a>")
+    x$user <- paste0("<a href=\"https://osm.org/user/", x$user, "\">", x$user, "</a>")
+    out <- knitr::kable(
+      x,
+      format = "html", row.names = FALSE, align = "c",
+      caption = if (titol_valid) paste0("<a href=\"", titol, "\">", titol, "</a>") else NULL,
+      escape = FALSE, table.attr = "cellpadding='10'; rules='rows'"
+    )
+  }
+
+  return(out)
+}
+
+
+#' Mostra les versions que introdueixen canvis en html
+#'
+#' Funció pensada per usar en fitxers `.qmd` o `.Rmd`.
+#'
+#' @param x Una llista de canvis retornada per [cerca_versio_canvis()].
+#'
+#' @return Mostra les taules html amb informació del conjunt de canvis que ha modificat etiquetes.
 #' @export
 #'
 #' @examples
-versio_canvis_html <- function(x, titol) {
-  x$changeset <- paste0("<a href=\"https://osm.org/changeset/", x$changeset, "\">", x$changeset, "</a>")
-  x$user <- paste0("<a href=\"https://osm.org/user/", x$user, "\">", x$user, "</a>")
-  out <- knitr::kable(
-    x,
-    format = "html", row.names = FALSE, align = "c",
-    caption = paste0("<a href=\"", titol, "\">", titol, "</a>"),
-    escape = FALSE, table.attr = "cellpadding='10'; rules='rows'"
-  )
-
-  return(out)
+versio_canvis_html <- function(x) {
+  for (i in seq_along(x)) {
+    if (nrow(x[[i]]) == 0) {
+      cat(versio_canvi_kable(x[[i]], titol = names(x)[i]), "\n\n<!-- -->\n\n")
+    } else {
+      print(versio_canvi_kable(x[[i]], titol = names(x)[i]))
+    }
+  }
+  invisible(x)
 }
