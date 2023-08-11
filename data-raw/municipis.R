6ef6fe4a9f2767a20686a97a56307cf63b3fd227
Joan Maspons <joanmaspons@gmail.com>
2023-08-11 12:52:12 +0200

    Millores i correccions de municipis comparant relacions amb llur admin_centre

--- data-raw/municipis.R
+++ data-raw/municipis.R
@@ -28,7 +28,73 @@ usethis::use_data(municipis, overwrite = TRUE, compress = "xz")
 # )
 
 
-## Afegeix els admin_center ----
+## Consulta dades a OSM ----
+
+municipis_osm <- consulta_etiquetes_osm(
+  x = municipis,
+  etiquetes = c("name:ca", "osm_id", "osm_type", "name", "wikidata", "wikipedia", "admin_level")
+)
+lapply(municipis_osm, unique)
+
+municipis <- municipis_osm[, c(
+  "name:ca", "regio", "comarca", "osm_id", "osm_type", "name", "wikipedia", "wikidata", "admin_level"
+)]
+
+
+## Compara nova base de dades amb la del paquet instal·lat ----
+
+library(compareDF)
+
+cols <- intersect(names(municipis), names(monitorOSM::municipis))
+diff_municipis <- compare_df(municipis[, cols], monitorOSM::municipis[, cols], group_col = c("osm_type", "osm_id"))
+view_html(diff_municipis)
+
+
+## Compara admin_center amb municipis ----
+
+municipis_rel <- municipis[municipis$osm_type == "relation", ]
+municipis_osm <- by(municipis_rel, municipis_rel$comarca, function(x) {
+  osmapiR::osm_fetch_objects(osm_type = "relation", osm_ids = x$osm_id)
+})
+
+admin_centre_list <- lapply(municipis_osm, function(x) {
+  admin_centres <- lapply(x$members, function(y) {
+    admin_centre <- y[y[, "role"] %in% "admin_centre", 1:2, drop = FALSE]
+    colnames(admin_centre) <- c("osm_type", "osm_id")
+    admin_centre
+  })
+
+  x$admin_centre <- admin_centres
+
+  return(x)
+})
+admin_centre <- do.call(rbind, admin_centre_list)
+
+municipis_uid <- do.call(paste, municipis[, c("osm_type", "osm_id")])
+grups <- list()
+for (i in seq_len(nrow(admin_centre))) {
+  x_uid <- do.call(paste, admin_centre[i, c("type", "id")])
+  if (nrow(admin_centre$admin_centre[[i]]) == 0) {
+    grups[[i]] <- municipis[municipis_uid %in% x_uid, ]
+  } else {
+    admin_centre_uid <- do.call(
+      paste,
+      as.data.frame(admin_centre$admin_centre[[i]][, c("osm_type", "osm_id"), drop = FALSE])
+    )
+
+    grups[[i]] <- rbind(
+      municipis[municipis_uid %in% x_uid, ],
+      municipis[municipis_uid %in% admin_centre_uid, ]
+    )
+  }
+}
+
+grups[sapply(grups, function(x) {
+  nrow(unique(x[, c("name:ca", "regio", "comarca", "name", "wikipedia", "wikidata")[1:4]])) # diferències en noms
+}) > 1]
+
+
+## FET: Afegeix els admin_center ----
 
 municipis_osm <- by(municipis, municipis$comarca, function(x) {
   osmapiR::osm_fetch_objects(osm_type = "relation", osm_ids = x$osm_id)
@@ -85,25 +151,3 @@ sum(nrow(municipis), nrow(municipis_admin_centre)) == nrow(municipis_centres)
 # dbTools::duplicatedPK(municipis_centres, pk = c("osm_type", "osm_id"))
 
 municipis <- municipis_centres
-
-
-## Consulta dades a OSM ----
-
-municipis_osm <- consulta_etiquetes_osm(
-  x = municipis,
-  etiquetes = c("name:ca", "osm_id", "osm_type", "name", "wikidata", "wikipedia", "admin_level")
-)
-lapply(municipis_osm, unique)
-
-municipis <- municipis_osm[, c(
-  "name:ca", "regio", "comarca", "osm_id", "osm_type", "name", "wikipedia", "wikidata", "admin_level"
-)]
-
-
-## Compara nova base de dades amb la del paquet instal·lat ----
-
-library(compareDF)
-
-cols <- intersect(names(municipis), names(monitorOSM::municipis))
-diff_municipis <- compare_df(municipis[, cols], monitorOSM::municipis[, cols], group_col = c("osm_type", "osm_id"))
-view_html(diff_municipis)
