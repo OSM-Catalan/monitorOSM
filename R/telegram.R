b5cd190e3dd8fac5d518813657a72c16b6462979
Joan Maspons <joanmaspons@gmail.com>
2023-08-12 18:02:44 +0200

    Notifica canvis al grup de Telegram de la Comunitat en català d'OpenStreetMap

---
+++ R/telegram.R
@@ -0,0 +1,19 @@
+#' Envia un missatge a al grup de Telegram de la `Comunitat en català d'OpenStreetMap`
+#'
+#' @param missatge Text del missatge.
+#' @param parse_mode El missatge s'interpreta com a `Markdown` o `HTML`.
+#'
+#' @return
+#' @export
+#'
+#' @examples
+missatge_telegram <- function(missatge = "Hi ha canvis a [monitorOSM](https://osm-catalan.github.io/monitorOSM).",
+                              parse_mode = "Markdown") {
+  bot <- telegram.bot::Bot(token = telegram.bot::bot_token("monitorOSM_bot"))
+
+  bot$sendMessage(
+    chat_id = Sys.getenv("TME_OSMCAT_CHATID"),
+    text = missatge,
+    parse_mode = parse_mode
+  )
+}
