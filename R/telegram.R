7aece32243fe6a664f8eacc2f74e31f282bf2bc1
Joan Maspons <joanmaspons@gmail.com>
2024-07-24 10:31:55 +0200

    Arregla warnings a R CMD check (caràcters no asci)

--- R/telegram.R
+++ R/telegram.R
@@ -1,4 +1,4 @@
-#' Envia un missatge a al grup de Telegram de la `Comunitat en català d'OpenStreetMap`
+#' Envia un missatge a al grup de Telegram de la `Comunitat en catal\u00E0 d'OpenStreetMap`
 #'
 #' @param missatge Text del missatge.
 #' @param parse_mode El missatge s'interpreta com a `Markdown` o `HTML`.
@@ -10,7 +10,10 @@
 missatge_telegram <- function(missatge = "Hi ha canvis a [monitorOSM](https://osm-catalan.github.io/monitorOSM).",
                               parse_mode = "Markdown") {
   if (!requireNamespace("telegram.bot", quietly = TRUE)) {
-    stop("El paquet `telegram.bot` no està instal·lat. Instal·leu-lo amb:\n\tinstall.packages(\"telegram.bot\")")
+    stop(
+      "El paquet `telegram.bot` no est\u00E0 instal\u00B7lat. Instal\u00B7leu-lo amb:\n",
+      "\tinstall.packages(\"telegram.bot\")"
+    )
   }
   bot <- telegram.bot::Bot(token = telegram.bot::bot_token("monitorOSM_bot"))
 
